d158f0aab3bb03399c77b70d02d2821d41fa2d19
Gabriel F <empresagabriel@gmail.com>
2020-02-27 20:01:41 -0300

    Substituing Eduardo's ex07 with Terra's ex07

--- java/ex07.java
+++ java/ex07.java
@@ -1,35 +1,32 @@
 import java.util.Scanner;
+
 public class ex07 {
- 
-    public static void main(String[] args) {
-        Scanner leTeclado;
-
-        int n=1, menor=0, maior=0, media=0, elementos=0;
-        leTeclado = new Scanner(System.in);
-
-        while(n>0){
-            System.out.println("Digite um numero:");
-
-            if (elementos == 0)
-                menor = n;
-
-            // n = 7, maior = 10, menor = 5
-        	if(n>maior){
-        		maior = n; 
-        	} 
-            else if (n<menor){
-        		menor = n;
-            }
-
-            if (n>0){
-                elementos+= 1;
-            }
-            media += n;
-            n = leTeclado.nextInt();
-        }
-    	System.out.println("O maior e " + maior + " e o menor e " + menor + ".");
-    	System.out.println("A quantidade de elementos e " + elementos);
-    	System.out.println("A media entre eles e" + (media/elementos));
-    	leTeclado.close();
-    }
+   public static void main(String[] args){
+      Scanner input = new Scanner(System.in);
+      System.out.println("digite um numero positivo diferente de 0. digite 0 ou um numero negativo se quiser encerrar o processo.");
+      float x = input.nextFloat();
+      float contador = 0;
+      float soma = 0;
+      float media = soma / 1;
+      float maior_numero = x;
+      float menor_numero = x;
+      while ( x > 0 ) {
+         contador ++;
+         soma += x;
+         media = soma / contador;
+         x = input.nextFloat();
+         if(x < menor_numero){
+            menor_numero = x;
+         }
+         if (x > maior_numero){
+            maior_numero = x;
+         }
+      }
+      System.out.println("maior valor: " + maior_numero +
+         "\nmenor valor: " + menor_numero +
+         "\nquantidade de valores: " + contador +
+         "\nsoma dos valores: " + soma +
+         "\nmedia dos valores: " + media );
+      System.out.println("processo encerrado");
+   }
 }
