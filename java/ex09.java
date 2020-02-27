4c59a6b42dd9a6f69b4d23bdfae5177ab1f07c7a
Gabriel F <empresagabriel@gmail.com>
2020-02-27 20:39:21 -0300

    Commiting ex09 as saw in livestream

--- java/ex09.java
+++ java/ex09.java
@@ -1,39 +1,41 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class ex09{
    
-   public static void main(String[] args){
-   Scanner leTeclado;
-   int Min=0, Max=0,X=1,Valores=0,Armazenar=0,i=0;
-   int vetor[] = new int[10];
-   leTeclado = new Scanner(System.in);
-   System.out.println("Digite um valor Min para o intervalo:");
-   Min = leTeclado.nextInt();
-   System.out.println("Digite um valor Max para o intervalo:");
-   Max = leTeclado.nextInt();
-   if (Min>Max){
-   	int Temporario=0;
-   	System.out.println("O valor Min é maior que o valor Max, o programa irá os inverter");
-   	Temporario = Min;
-   	Min = Max;
-   	Max = Temporario;
-   }
+    public static void main(String[] args){
+        Scanner leTeclado;
+        int Min=0, Max=0,X=1,Valores=0,Armazenar=0,i=0;
+        int vetor[] = new int[10];
+        leTeclado = new Scanner(System.in);
+        System.out.println("Digite um valor Min para o intervalo:");
+        Min = leTeclado.nextInt();
+        System.out.println("Digite um valor Max para o intervalo:");
+        Max = leTeclado.nextInt();
+        if (Min>Max){
+            int Temporario=0;
+            System.out.println("O valor Min e maior que o valor Max, o programa ira os inverter");
+            Temporario = Min;
+            Min = Max;
+            Max = Temporario;
+        }
 
-   while (X!=0){
-   System.out.println("Digite um valor inteiro para X, e 0 quando tiver colocado todos os valores desejados: ");
-   X = leTeclado.nextInt();
-   		if ((X>Min) && (X<Max)){
-   			System.out.println("X situa-se no intervalo [Min,Max], adicionado na totalização");
-            vetor[i] = X;
-   			Valores++;
-            Armazenar+= X;
-            i++;
-   		}
-   		else 
-   			System.out.println("Valor fora do intervalo [Min, Max], ignorado na totalização");
+        while (X!=0){
+            System.out.println("Digite um valor inteiro para X, e 0 quando tiver colocado todos os valores desejados: ");
+            X = leTeclado.nextInt();
+            if ((X>Min) && (X<Max)){
+                System.out.println("X situa-se no intervalo [Min,Max], adicionado na totalizacao");
+                vetor[i] = X;
+                Valores++;
+                Armazenar+= X;
+                i++;
+            }
+            else
+                System.out.println("Valor fora do intervalo [Min, Max], ignorado na totalizacao");
 
-   }
-   System.out.println("Os valores fornecidos foram " + Valores + ".");
-   System.out.println("A soma dos valores fornecidos é " + Armazenar + ".");
-   System.out.println("Os numeros fornecidos foram " + vetor + ".");
-   }
+        }
+        System.out.println("Os valores fornecidos foram " + Valores + ".");
+        System.out.println("A soma dos valores fornecidos e " + Armazenar + ".");
+        System.out.println("Os numeros fornecidos foram " + Array.toString(vetor) + ".");
+    }
 }
