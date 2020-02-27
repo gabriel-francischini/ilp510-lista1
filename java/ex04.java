5ee38fd5723f4957ca951ff831a43dd9307f091c
Gabriel F <empresagabriel@gmail.com>
2020-02-27 19:30:23 -0300

    Commiting new changes according to livestream

--- java/ex04.java
+++ java/ex04.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ex04{
+public class ex04{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Digite o primeiro numero real positivo");
@@ -10,9 +10,9 @@ public class Ex04{
         System.out.print("Digite o terceiro numero real positivo");
         float c = input.nextFloat();
 
-        boolean AisTriangular = ( ( a < (b + c) ) && ( a > (b - c) ) && ( a > (c - b))) ;
-        boolean BisTriangular = ( (b < (a + c)) && ( b > (a - c)) && (b > (c - a))) ;
-        boolean CisTriangular = ( (c < (b + a)) && (c > (b - a)) && (c > (a - b)));
+        boolean AisTriangular = (a < (b + c)) && (a > Math.abs(b - c));
+        boolean BisTriangular = (b < (a + c)) && (b > Math.abs(a - c));
+        boolean CisTriangular = (c < (b + a)) && (c > Math.abs(b - a));
 
         if (AisTriangular && BisTriangular && CisTriangular){
             System.out.println("Os numeros formam um triangulo");
